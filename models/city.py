542bf980c62d08a371e29802d7a74eaf9f7742f4
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-09-18 20:15:58 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -2,13 +2,13 @@
 """ City Module for HBNB project """
 from os import environ
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String, ForeignKey
 # from models.state import State
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     if environ.get("HBNB_TYPE_STORAGE") == "db":
         __tablename__ = 'cities'
