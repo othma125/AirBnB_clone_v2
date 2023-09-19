dfca7e73009a596efda739e0bbc30021b934e557
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-09-19 15:50:34 +0100

    tests/test_models/test_base_model.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
 from models.base_model import BaseModel
 from models.city import City
 from models.state import State
@@ -10,6 +8,8 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 from os import environ
+from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
 
 
 storage_engine = environ.get("HBNB_TYPE_STORAGE")
