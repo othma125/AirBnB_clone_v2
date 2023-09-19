35c00fc7cc12e0992ad7345d09e8c32451a32b9f
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-09-19 15:53:39 +0100

    models/review.py

--- models/review.py
+++ models/review.py
@@ -2,6 +2,8 @@
 """ Review module for the HBNB project """
 from os import environ
 
+from sqlalchemy.orm import relationship
+
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 
@@ -14,6 +16,7 @@ class Review(BaseModel, Base):
         place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
         user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
         text = Column(String(1024), nullable=False)
+        place = relationship("Place", back_populates="reviews")
     else:
         place_id = ""
         user_id = ""
