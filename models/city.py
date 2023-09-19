0529aa9781af3bb5e7739586e9942dd5234d305a
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-09-19 15:17:23 +0100

    models/city.py

--- models/city.py
+++ models/city.py
@@ -5,14 +5,14 @@ from os import environ
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String, ForeignKey
-# from models.state import State
+from models.state import State
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     if environ.get("HBNB_TYPE_STORAGE") == "db":
         __tablename__ = 'cities'
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        state_id = Column(String(60), ForeignKey(State.id), nullable=False)
         name = Column(String(128), nullable=False)
         places = relationship("Place", backref="cities")
     else:
