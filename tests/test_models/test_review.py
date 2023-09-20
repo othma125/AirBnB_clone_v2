9f52ed52154d5f81c1f65d41cdbe383f0697dc2a
bensbahou <younesbensbahou@gmail.com>
2023-09-20 13:44:05 +0100

    doc test review

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -14,16 +14,16 @@ class test_review(test_basemodel):
         self.value = Review
 
     def test_place_id(self):
-        """ """
+        """ test place id """
         new = self.value()
         self.assertEqual(type(new.place_id), str)
 
     def test_user_id(self):
-        """ """
+        """ test user id """
         new = self.value()
         self.assertEqual(type(new.user_id), str)
 
     def test_text(self):
-        """ """
+        """ test text """
         new = self.value()
         self.assertEqual(type(new.text), str)
