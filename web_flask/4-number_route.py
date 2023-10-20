46b08ae67dd05efb8364efc741391059ccb77e5d
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-10-20 14:40:47 +0100

    web_flask/6-number_odd_or_even.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -17,7 +17,7 @@ def hello():
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """returns Hello HBNB!"""
-    return 'HBNB!'
+    return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
