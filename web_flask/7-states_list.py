bd2b0cd42c622021a54e433c22d6be29689e2abb
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-10-21 10:54:56 +0100

    7-states_list.py

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""list of states"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+	"""display a HTML page"""
+	states = storage.all(State)
+	return render_template('7-states_list.html', states=states)
