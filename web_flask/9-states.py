e1cd8ff3c8cb88452313f02b1014c9a231bb8b56
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-10-21 14:48:22 +0100

    9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -8,13 +8,13 @@ app = Flask(__name__)
 
 
 @app.route('/states', strict_slashes=False)
-@app.route('/states/<id>', strict_slashes=False)
-def states(id=None):
+@app.route('/states/<state_id>', strict_slashes=False)
+def states(state_id=None):
     """display a HTML page that lists states"""
-    states = storage.all(State)
-    if id:
-        id = 'State.' + id
-    return render_template('9-states.html', states=states, id=id)
+    states_dict = storage.all(State)
+    if state_id:
+        state_id = 'State.' + state_id
+    return render_template('9-states.html', states=states_dict, id=state_id)
 
 
 @app.teardown_appcontext
