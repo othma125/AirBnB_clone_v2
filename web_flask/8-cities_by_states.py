fa1a17cc1fae965bf4f19091e9c5b844f2b41bb6
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-10-21 13:27:07 +0100

    8-cities_by_states.py

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""list of cities and states"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """display a HTML page"""
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states.values())
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
