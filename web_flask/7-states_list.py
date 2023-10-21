44a84fb2e71019c5152baf3796d4706a9d37a0f2
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-10-21 12:55:47 +0100

    7-dump.sql

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,7 +2,7 @@
 """list of states"""
 from flask import Flask, render_template
 from models import storage
-from models.state import state
+from models.state import State
 app = Flask(__name__)
 
 
@@ -11,3 +11,7 @@ def states_list():
 	"""display a HTML page"""
 	states = storage.all(State)
 	return render_template('7-states_list.html', states=states.values())
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
