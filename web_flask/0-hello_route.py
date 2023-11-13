1db86e6ddc037bae19c7f8c1686dd2a06eea172d
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-11-13 13:19:26 +0100

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
+@app.route('airbnb-onepage', strict_slashes=False)
 def hello():
     """returns Hello HBNB!"""
     return 'Hello HBNB!'
